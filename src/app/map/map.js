e02b00ac7aec8161ab2b0102c748634a94f943fe
kayoshi <jo5cha@web.de>
2024-05-02 16:12:24 +0200

    cluster

--- src/app/map/map.js
+++ src/app/map/map.js
@@ -1,15 +1,31 @@
 // this is necessary since leaflet is not server-side compatible
 "use client"
 
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, Source, Layer } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function CasesMap({ cases }) {
+  const geojson = {
+    type: 'FeatureCollection',
+    features: cases.map((c) => ({
+      type: 'Feature',
+      properties: { ...c },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          c.address?.coordinates?.coordinates?.lng || 0,
+          c.address?.coordinates?.coordinates?.lat || 0,
+        ],
+      },
+    })),
+  };
+
   return <div>
     <Map
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
       mapLib={import('mapbox-gl')}
       attributionControl={false}
+      maxZoom={12}
       initialViewState={{
         longitude: 11,
         latitude: 52,
@@ -18,17 +34,59 @@ export default function CasesMap({ cases }) {
       style={{width: "100%", height: "70vh"}}
       mapStyle="mapbox://styles/jo5cha/clv2eb5jl00c301qr7qdua42f"
     >
-      {cases.map((c) => (
-        <Marker
-          onClick={() => console.log(c)}
-          key={c.id}
-          longitude={c.address?.coordinates?.coordinates?.lng || 0}
-          latitude={c.address?.coordinates?.coordinates?.lat || 0}
-          anchor="bottom"
-          color="black"
+       <Source
+          id="cases"
+          type="geojson"
+          data={geojson}
+          cluster={true}
+          clusterMaxZoom={14}
+          clusterRadius={100}
         >
-        </Marker>
-      ))}
+          <Layer
+            id="clusters"
+            type="circle"
+            source="cases"
+            filter={['has', 'point_count']}
+            paint={{
+              'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#d900a3', 5,
+                '#d90094', 10,
+                '#d9004c',
+              ],
+              'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20, 5,
+                30, 10,
+                40,
+              ],
+            }}
+          />
+          <Layer
+            id="cluster-count"
+            type="symbol"
+            source="cases"
+            filter={['has', 'point_count']}
+            layout={{
+              'text-field': '{point_count_abbreviated}',
+              'text-size': 12,
+              'color': 'white'
+            }}
+          />
+          <Layer
+            id="unclustered-point"
+            type="circle"
+            source="cases"
+            filter={['!', ['has', 'point_count']]}
+            paint={{
+              'circle-color': '#d900a3',
+              'circle-radius': 10,
+              'circle-stroke-width': 0
+            }}
+          />
+        </Source>
     </Map>
   </div>
 }
